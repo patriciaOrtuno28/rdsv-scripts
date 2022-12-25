2536d11099ea19031188cd309803b7a16765c90d
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-12-25 20:03:12 +0100

    Fixed bug

--- renes/handlePods.sh
+++ renes/handlePods.sh
@@ -3,10 +3,13 @@
 # Definir las variables de entorno de los pods ACCESS y CPE
 echo "Definiendo CPEPOD ..."
 AUXCPE=$(kubectl -n $OSMNS get pods | grep helmchartrepo-cpechart-)
-CPEPOD=${AUXCPE:0:49}
+CPEPOD=${AUXCPE:0:50}
 echo $CPEPOD
 
-echo "Definiendo CPEPOD ..."
+echo "Definiendo ACCPOD ..."
 AUXACC=$(kubectl -n $OSMNS get pods | grep helmchartrepo-accesschart-)
 ACCPOD=${AUXACC:0:53}
 echo $ACCPOD
+
+# Enseñar vxlanacc
+kubectl -n $OSMNS exec -it $ACCPOD -- /bin/bash
