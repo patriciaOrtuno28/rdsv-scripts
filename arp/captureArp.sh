aef1f41ab7d21ed165b17f1401f9fad71d1f6a31
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-12-30 19:46:59 +0100

    Added new interfaces to arpwatch

--- arp/captureArp.sh
+++ arp/captureArp.sh
@@ -6,23 +6,28 @@ echo 'deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted mult
 apt-get update
 
 ## TODO: Add -y flag
-apt-get -f install sysv-rc-conf
+apt-get -f install sysv-rc-conf -y
 
 alias chkconfig=sysv-rc-conf
 
 chkconfig --level 35 arpwatch on
 
-sed -i 's@INTERFACES=""@INTERFACES="net1"@g' /etc/default/arpwatch
+sed -i 's@INTERFACES=""@INTERFACES="net1 eth0 brint"@g' /etc/default/arpwatch
 
-/etc/init.d/arpwatch start
-
-arpwatch -i net1
-
+echo ""
 echo "Configurando la direccion de correo electronico ..."
-echo 'IFACE_ARGS="-m patiortu@gmail.com"' > /etc/arpwatch/net1.iface
+echo 'IFACE_ARGS="-m p.ortuno@alumnos.upm.es"' > /etc/arpwatch/net1.iface
+echo 'IFACE_ARGS="-m p.ortuno@alumnos.upm.es"' > /etc/arpwatch/eth0.iface
+echo 'IFACE_ARGS="-m p.ortuno@alumnos.upm.es"' > /etc/arpwatch/brint.iface
 
+echo ""
+echo "Iniciando el servicio ARP ..."
+etc/init.d/arpwatch start
+
+echo ""
+echo "Verificando proceso ARP en ejecucion ..."
+ps -ef |grep arpwatch
+
+echo ""
 echo "Verificar tabla ARP actual: "
 arp -a
-
-#echo "Contenido inicial de net1.dat: "
-#cat /var/lib/arpwatch/net1.dat
