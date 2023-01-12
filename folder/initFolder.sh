e22ba18fc4c57e96c9dd7b26a466f6530c873a82
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-12 21:41:18 +0100

    New files to get ips

--- folder/initFolder.sh
+++ folder/initFolder.sh
@@ -18,5 +18,8 @@ cp ../nfv-lab/osm_renes_start.sh .
 cp ../nfv-lab/osm_renes1.sh .
 cp ../rdsv-scripts/folder/osm_renes2.sh .
 
+yes | cp -rf ../rdsv-scripts/scenario/nfv3_home_lxc_ubuntu64.xml vnx/nfv3_home_lxc_ubuntu64.xml
+yes | cp -rf ../rdsv-scripts/scenario/nfv3_server_lxc_ubuntu64.xml vnx/nfv3_server_lxc_ubuntu64.xml
+
 # Clonar el repositorio repo-rdsv con las imágenes propias
 if cd ~/shared/rdsv-final/repo-rdsv; then git pull; else git clone https://github.com/patriciaOrtuno28/repo-rdsv.git ~/shared/rdsv-final/repo-rdsv; fi
--- scenario/initScenario.sh
+++ scenario/initScenario.sh
@@ -3,3 +3,9 @@
 cd /home/upm/shared/rdsv-final
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
+
+# Asegurarse de que se han configurado las ips
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h11
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h12
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h21
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h22
