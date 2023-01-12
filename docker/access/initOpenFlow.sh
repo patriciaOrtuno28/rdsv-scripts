79ad84ed7292716769e0e90ca309cf99c970c76c
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-12 22:15:13 +0100

    Automated initOpenFlow.sh

--- docker/access/initOpenFlow.sh
+++ docker/access/initOpenFlow.sh
@@ -1,53 +1,63 @@
 #!/bin/bash
 
-# Instalar dependencias necesarias
-echo "Instalando dependencias ..."
-cd ryu/
-pip install .
-cd ..
+while getopts a:b: flag
+do
+    case "${flag}" in
+        a) hx1=${OPTARG};;
+        b) hx2=${OPTARG};;
+    esac
+done
 
-# Para activar OpenFlow version 1.3 en el bridge brint, porque sino se pone por defecto la v1.0:
-echo "Definiendo version OpenFlow 1.3 ..."
-ovs-vsctl set Bridge brint protocols=OpenFlow13
+if [[ $hx1 == "" ]] || [[ $hx2 == "" ]]
+    echo "Se deben definir las IPs de la red residencial"
+else
+    # Instalar dependencias necesarias
+    echo "Instalando dependencias ..."
+    cd ryu/
+    pip install .
+    cd ..
 
-# Definir el puerto del manager de OpenFlow:
-echo "Definiendo propiedades del controller ..."
-ovs-vsctl set-manager ptcp:6632
-ovs-vsctl set-controller brint tcp:127.0.0.1:6633
-ovs-vsctl set bridge brint other-config:datapath-id=0000000000000001
+    # Para activar OpenFlow version 1.3 en el bridge brint, porque sino se pone por defecto la v1.0:
+    echo "Definiendo version OpenFlow 1.3 ..."
+    ovs-vsctl set Bridge brint protocols=OpenFlow13
 
-# Crear qos_simple_switch_13.py
-echo "Creando qos_simple_switch_13.py ..."
-sed '/OFPFlowMod(/,/)/s/)/, table_id=1)/' ryu/ryu/app/simple_switch_13.py > ryu/ryu/app/qos_simple_switch_13.py
+    # Definir el puerto del manager de OpenFlow:
+    echo "Definiendo propiedades del controller ..."
+    ovs-vsctl set-manager ptcp:6632
+    ovs-vsctl set-controller brint tcp:127.0.0.1:6633
+    ovs-vsctl set bridge brint other-config:datapath-id=0000000000000001
 
-# Instalar dependencias
-echo "Instalando dependencias ..."
-cd ryu/; python3 ./setup.py install
-cd ..
+    # Crear qos_simple_switch_13.py
+    echo "Creando qos_simple_switch_13.py ..."
+    sed '/OFPFlowMod(/,/)/s/)/, table_id=1)/' ryu/ryu/app/simple_switch_13.py > ryu/ryu/app/qos_simple_switch_13.py
 
-# Para ejecutar la aplicacion Ryu:
-echo "Ejecutando aplicacion Ryu qos_simple_switch_13.py ..."
-ryu-manager ryu/ryu/app/rest_qos.py ryu/ryu/app/qos_simple_switch_13.py ryu/ryu/app/rest_conf_switch.py &
+    # Instalar dependencias
+    echo "Instalando dependencias ..."
+    cd ryu/; python3 ./setup.py install
+    cd ..
 
-# Terminates the program (like Ctrl+C)
-PID=$!
-sleep 5
-kill -INT $PID
+    # Para ejecutar la aplicacion Ryu:
+    echo "Ejecutando aplicacion Ryu qos_simple_switch_13.py ..."
+    ryu-manager ryu/ryu/app/rest_qos.py ryu/ryu/app/qos_simple_switch_13.py ryu/ryu/app/rest_conf_switch.py &
 
-# KNF access -> brgX : 12 Mbps bajada
-# brgX -> KNF access : 6 Mbps subida
-# Definir la ruta del manager
-curl -X PUT -d '"tcp:127.0.0.1:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
-sleep 2
+    # Terminates the program (like Ctrl+C)
+    PID=$!
+    sleep 5
+    kill -INT $PID
 
-# Crear una cola con QoS
-# 12 Mbps como máximo para el enlace
-# Cola 0 - hx1 : mínimo 8 Mbps
-# Cola 1 - hx2 : máximo 4 Mbps
-curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
+    # KNF access -> brgX : 12 Mbps bajada
+    # brgX -> KNF access : 6 Mbps subida
+    # Definir la ruta del manager
+    curl -X PUT -d '"tcp:127.0.0.1:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
+    sleep 2
 
-# Definir a que cola pertenece cada tráfico
-# eth1 : hx1
-# eth2: hx2
-curl -X POST -d '{"match": {"nw_dst": "192.168.255.21"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
-curl -X POST -d '{"match": {"nw_dst": "192.168.255.20"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
+    # Crear una cola con QoS
+    # 12 Mbps como máximo para el enlace
+    # Cola 0 - hx1 : mínimo 8 Mbps
+    # Cola 1 - hx2 : máximo 4 Mbps
+    curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
+
+    # Definir a que cola pertenece cada tráfico
+    curl -X POST -d '{"match": {"nw_dst": $hx1}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
+    curl -X POST -d '{"match": {"nw_dst": $hx2}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
+fi
