3938a64953bb17a58252d1cef76fbc63325a966e
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-12 21:55:34 +0100

    Added osm_renes1.sh to folder

--- folder/osm_renes1.sh
+++ folder/osm_renes1.sh
@@ -2,16 +2,16 @@
 export OSMNS  # needs to be defined in calling shell
 
 # service instance name
-export SINAME="renes2"
+export SINAME="renes1"
 
 # HOMETUNIP: the ip address for the home side of the tunnel
-export HOMETUNIP="10.255.0.4"
+export HOMETUNIP="10.255.0.2"
 
 # VNFTUNIP: the ip address for the vnf side of the tunnel
-export VNFTUNIP="10.255.0.3"
+export VNFTUNIP="10.255.0.1"
 
 # VCPEPUBIP: the public ip address for the vcpe
-export VCPEPUBIP="10.100.1.2"
+export VCPEPUBIP="10.100.1.1"
 
 # VCPEGW: the default gateway for the vcpe
 export VCPEGW="10.100.1.254"
