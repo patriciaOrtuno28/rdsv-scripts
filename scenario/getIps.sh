c4ad7e55b73620625954169e1a80472c770fc546
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-13 21:00:03 +0100

    Fusionado lógica getIps en un fichero

--- scenario/getIps.sh
+++ scenario/getIps.sh
@@ -2,6 +2,13 @@
 
 cd /home/upm/shared/rdsv-final
 
+# Asegurarse de que se han configurado las ips
+echo "Ejecutando dhclient en los equipos de la red residencial ..."
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h11
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h12
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h21
+sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h22
+
 echo "IP h11: "
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x retrieve_ip_h11 | grep 192.168.255
 
--- scenario/setIps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Asegurarse de que se han configurado las ips
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h11
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h12
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h21
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x dhclient_h22
