af83ef52f4fe8f18db9c5cbf82da300aaac3df8b
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-15 11:39:34 +0100

    Instalar iperf3

--- kubernetes/initScenario.sh
+++ kubernetes/initScenario.sh
@@ -4,14 +4,3 @@
 cd /home/upm/shared/rdsv-final
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
-
-# Instalar iperf3
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h11
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h12
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h21
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h22
-
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_brg1
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_brg2
-
-sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -x iperf3_s1
