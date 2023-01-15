bcfad38237e38d30a9bc58a1f4245d3bdcc5ff8e
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-15 11:54:34 +0100

    Included deleting onboarding in destroyRenes file

--- kubernetes/modifyRootFs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo vnx --modify-rootfs /usr/share/vnx/filesystems/vnx_rootfs_lxc_ubuntu64-20.04-v025-vnxlab/
--- osm/destroyRenes.sh
+++ osm/destroyRenes.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-# Obtener el renes que hay que eliminar
-while getopts r: flag
-do
-    case "${flag}" in
-        r) renes=${OPTARG};;
-    esac
-done
 
-# Borrar una instancia de renes
-if [[ $renes == "1" ]]
-then 
-    echo "Deleting renes1 ..."
-    osm ns-delete $NSID1
-elif [[ $renes == "2" ]]
-then
-    echo "Deleting renes2 ..."
-    osm ns-delete $NSID2
-else
-    echo "Deleting renes1 and renes2 ..."
-    osm ns-delete $NSID1
-    osm ns-delete $NSID1
-fi
+echo "Borrando el repositorio de K8s ..."
+osm repo-delete helmchartrepo
 
-# echo "Deleting pods ..."
-# kubectl delete -n $OSMNS deployment --all
+echo "Borrando renes1 y renes2 ..."
+osm ns-delete $NSID1
+osm ns-delete $NSID1
+
+sleep 30
+
+echo "Borrando el NS Package ..."
+osm nspkg-delete renes
+
+sleep 5
+
+echo "Borrando los VNF Package ..."
+osm nfpkg-delete accessknf
+osm nfpkg-delete cpeknf
+
