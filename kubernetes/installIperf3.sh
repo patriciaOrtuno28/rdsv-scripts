68ff9fd48cb1f0a3b39b5ee6149a91e30f5e6db5
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-16 13:52:05 +0100

    MIssing cd instruction in the iperf3 installation script

--- kubernetes/installIperf3.sh
+++ kubernetes/installIperf3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/upm/shared/rdsv-final
+
 # Instalar iperf3
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h11
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h12
