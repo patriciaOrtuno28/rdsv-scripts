64eabbec891d905b867c7a05998d7ccaae290b76
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-31 19:40:08 +0100

    Erases unnecesary iperf3 commands

--- kubernetes/installIperf3.sh
+++ kubernetes/installIperf3.sh
@@ -7,8 +7,3 @@ sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h11
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h12
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h21
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_h22
-
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_brg1
-sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -x iperf3_brg2
-
-sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -x iperf3_s1
