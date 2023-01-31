264c16141e63798b7924085a19dce1721ab5c086
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-31 22:29:25 +0100

    Comment

--- osm/upQos.sh
+++ osm/upQos.sh
@@ -24,6 +24,7 @@ if [[ ! $VACC1 =~ "helmchartrepo-accesschart"  ]]; then
     echo "ERROR: incorrect <access_deployment_id>: $VACC1"
     exit 1
 fi
+echo $VACC1
 
 echo "Definiendo ACCPOD para renes2 ..."
 OSMACC2=$(deployment_id renes2 "access")
@@ -33,36 +34,37 @@ if [[ ! $VACC2 =~ "helmchartrepo-accesschart"  ]]; then
     echo "ERROR: incorrect <access_deployment_id>: $VACC2"
     exit 1
 fi
+echo $VACC2
 
 ACC_EXEC_1="kubectl exec -n $OSMNS $VACC1 --"
 ACC_EXEC_2="kubectl exec -n $OSMNS $VACC2 --"
 
 echo "Definiendo QoS de subida para renes1 ..."
 # Definir la ruta del manager
-$ACC_EXEC_1 curl -X PUT -d '"tcp:10.255.0.2:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000002/ovsdb_addr
+$ACC_EXEC_1 curl -X PUT -d '"tcp:10.255.0.2:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
 sleep 2
 
 # Crear una cola con QoS
 # 6 Mbps como máximo para el enlace
 # Cola 0 - hx1 : máx 2 Mbps
 # Cola 1 - hx2 : mín 4 Mbps
-$ACC_EXEC_1 curl -X POST -d '{"port_name": "vxlan1", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000002
+$ACC_EXEC_1 curl -X POST -d '{"port_name": "vxlanint", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
 
 # Definir a que cola pertenece cada tráfico
-$ACC_EXEC_1 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:01:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000002
-$ACC_EXEC_1 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:00:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000002
+$ACC_EXEC_1 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:01:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
+$ACC_EXEC_1 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:00:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
 
-
+echo "Definiendo QoS de subida para renes2 ..."
 # Definir la ruta del manager
-$ACC_EXEC_2 curl -X PUT -d '"tcp:10.255.0.4:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000002/ovsdb_addr
+$ACC_EXEC_2 curl -X PUT -d '"tcp:10.255.0.4:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
 sleep 2
 
 # Crear una cola con QoS
 # 6 Mbps como máximo para el enlace
 # Cola 0 - hx1 : máx 2 Mbps
 # Cola 1 - hx2 : mín 4 Mbps
-$ACC_EXEC_2 curl -X POST -d '{"port_name": "vxlan1", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000002
+$ACC_EXEC_2 curl -X POST -d '{"port_name": "vxlanint", "type": "linux-htb", "max_rate": "6000000", "queues": [{"max_rate": "2000000"}, {"min_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
 
 # Definir a que cola pertenece cada tráfico
-$ACC_EXEC_2 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:04:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000002
-$ACC_EXEC_2 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:03:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000002
+$ACC_EXEC_2 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:04:01", "dl_type": "IPv4"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
+$ACC_EXEC_2 curl -X POST -d '{"match": {"dl_src": "02:fd:00:04:03:01", "dl_type": "IPv4"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
